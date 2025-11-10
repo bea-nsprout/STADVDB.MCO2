5cb58f1452c4cce736e36828d51890043fc61eb9
bea-nsprout <beaantoinette26@gmail.com>
2025-11-10 19:54:34 +0800

    Added passing info through url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,37 @@
 	import TrainMap from '$lib/components/TrainMap.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import SearchFilter from '$lib/components/SearchFilter.svelte';
-	import Input from '$lib/components/Input.svelte';
-	import ClassType from '$lib/components/book-train/ClassType.svelte';
 	import Row from '$lib/components/book-train/Row.svelte';
 
 	let stationsMenu = ["Tokyo", "Shin-Yokohoma", "Toyohashi", "Nagoya", "Kyoto", "Shin-Osaka"]
-	let stationFrom: string;
-	let stationTo: string;
+	let stationFrom = $state("");
+	let stationTo = $state("");
+
+	let trains =
+	[
+		{
+			id: "1000",
+			name: "Thomas the Train",
+			departs: "2:31 AM",
+			arrives: "3:59 AM",
+			capacity: [18, 64, 80]
+		},
+		{
+			id: "1001",
+			name: "Thomas the Train",
+			departs: "3:32 AM",
+			arrives: "4:53 AM",
+			capacity: [18, 64, 80]
+		},
+		{
+			id: "1002",
+			name: "Thomas the Train",
+			departs: "3:32 AM",
+			arrives: "4:53 AM",
+			capacity: [18, 64, 80]
+		}
+	]
+	let psngrCount = $state(0);
 </script>
 
 <Header />
@@ -19,9 +43,9 @@
 
 		<div class="flex flex-row ">
 		<span class="label"><b>Station</b></span>
-			<div class="flex flex-row w-[620px] gap-auto">
+			<div class="flex flex-wrap w-[620px] gap-auto">
 				<SearchFilter fieldname="From" menuItems={stationsMenu} bind:value={stationFrom}/>
-				<span class="flex-1 min-w-0">--</span>
+				<span class="flex-1 min-w-0"><i class="bi bi-arrow-right"></i></span>
 				<SearchFilter fieldname="To" menuItems={stationsMenu} bind:value={stationTo}/>
 			</div>
 		</div>
@@ -31,7 +55,7 @@
 		<div class="flex flex-row ">
 			<span class="label"><b>Schedule Filter</b></span>
 
-			<div class="flex flex-row w-[620px] gap-2">
+			<div class="flex flex-wrap w-[620px] gap-2">
 				<select class="flex-1 min-w-0 !py-0">
 					<option>Departs at</option>
 					<option>Arrives at</option>
@@ -47,7 +71,7 @@
 			<span class="label"><b>Passenger Count</b></span>
 
 			<div class="flex flex-row w-[620px] gap-2">
-				<input type="number" min="1" max="6">
+				<input type="number" min="1" max="6" bind:value={psngrCount}>
 			</div>
 		</div>
 
@@ -63,21 +87,10 @@
 			<span class="flex-2 flex flex-col gap-1">Capacity</span>
 		</section>
 
-		<Row train={
-			{name: "Thomas the Train",
-			departs: "2:31 AM",
-			arrives: "3:59 AM",
-			capacity: [18, 64, 80]}} />
-		<Row train={
-			{name: "Thomas the Train",
-			departs: "2:31 AM",
-			arrives: "3:59 AM",
-			capacity: [18, 64, 80]}} />
-		<Row train={
-			{name: "Thomas the Train",
-			departs: "2:31 AM",
-			arrives: "3:59 AM",
-			capacity: [18, 64, 80]}} />
+		{#each trains as train}
+			<Row {train} urlInfo={{train: train.id, psngrCount: psngrCount, from: stationFrom, to: stationTo}} />
+		{/each}
+
 
 	</section>
 </article>
