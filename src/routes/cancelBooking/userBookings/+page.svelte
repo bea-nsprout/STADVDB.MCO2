c5ba673300af2896d5b56b4d37f9da7f6d7a6153
unknown <riverap1231@gmail.com>
2025-11-11 21:56:08 +0800

    added delete modal

--- src/routes/cancelBooking/userBookings/+page.svelte
+++ src/routes/cancelBooking/userBookings/+page.svelte
@@ -1,8 +1,32 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+
 <script lang="ts">
 	import Header from '$lib/components/Header.svelte';
     import TrainMap from '$lib/components/TrainMap.svelte';
-</script>
 
+    let cancelledBooking = 0;
+    let modal: HTMLElement | null = null;
+
+    function openModal(bookingNumber: number) {
+        cancelledBooking = bookingNumber;
+        if (modal) {
+            modal.style.display = "block";
+        }
+    }
+
+    
+    function deleteBooking() {
+        // Placeholder for delete booking logic
+        if (modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>
+<svelte:window on:click={(event) => {
+    if (modal && event.target === modal) {
+        modal.style.display = "none";
+    }
+}}/>
 <div> <!--Container Div-->
    <Header/>
    <div class = "flex flex-row gap-x-30">
@@ -12,9 +36,14 @@
                 [USERNAME]'s Bookings
             </div> 
             <div class = "flex flex-col gap-y-5"> <!--Bookings List-->
-                <div class = "Booking"> <!--Booking-->
-                    <div>
-                        Booking 1
+                <div class = "Booking"> <!--Booking needs to be looped once db is available-->
+                    <div class = "flex flex-row">
+                        <div class = "bookinglabel">
+                            Booking 1
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-trash" on:click = {() => openModal(1)} role = "button"/>
+                        </div>
                     </div>
                     <div class = "text-[var(--color-accent)] font-bold text-3xl flex flex-row">
                         <div class = "station-from">
@@ -54,9 +83,22 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 
-                
-
+    <div bind:this={modal} class = "modal">
+        <div class = "modalcontent">
+            <div class = "text-5xl font-bold">
+                Cancel <span class = "text-[var(--color-accent)]"> Booking {cancelledBooking}? </span>
+            </div>
+            <div>
+                <button class = "bg-red-500 text-white font-bold px-4 py-2 rounded mr-4" on:click = {() => deleteBooking()} id ="delete">
+                    Delete Booking
+                </button>
+                <button class = "bg-green-300 text-white font-bold px-4 py-2 rounded" on:click = {() => {if (modal) {modal.style.display = "none";}}} id= "keep">
+                    Keep Booking
+                </button>
+            </div>
         </div>
     </div>
 </div>
@@ -81,6 +123,80 @@
     .station-to {
        width:40%;
        text-align: right;
+    }
+
+    .bookinglabel {
+        width:100%;
+    }
+
+    .modal {
+        position:fixed;
+        z-index: 1;
+        right: 0;
+        top:0;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        background-color: rgb(0,0,0);
+        background-color: rgba(0,0,0,0.1);
+        --webkit-animation-name: fadeIn;
+        --webkit-animation-duration: 0.4s;
+        animation-name:fadeIn;
+        animation-duration: 0.4s;
+        display:none;
+    }
+
+    .modalcontent {
+       display:flex;
+       flex-direction: column;
+       gap:50px;
+       position: fixed;
+       padding: 30px;
+       right: 0;
+       bottom:0;
+       background-color: #fefefe;
+       width: 50%;
+       -webkit-animation-name: slideIn;
+       -webkit-animation-duration: 0.4s;
+        animation-name: slideIn;
+        animation-duration: 0.4s;
+        border-radius: 15px 0px 0px ;
+        box-shadow: 1px 1px 10px;
+    }
+
+    #keep:hover {
+        cursor: pointer;
+        box-shadow: 5px 5px 5px 2px inset #6AB175;
+    }
+
+    #delete:hover {
+        cursor: pointer;
+        box-shadow: 5px 5px 5px 2px inset #C22B2E;
+    }
+
+    i:hover {
+        cursor: pointer;
+        color: red;
+    }
+
+    @-webkit-keyframes slideIn {
+        from {bottom: -300px; opacity: 0} 
+        to {right: 0; opacity: 1}
+    } 
+
+    @keyframes slideIn {
+        from {bottom: -300px; opacity: 0}
+        to {right: 0; opacity: 1}
+    }
+
+    @-webkit-keyframes fadeIn {
+        from {opacity: 0} 
+        to {opacity: 1}
+    }
+
+    @keyframes fadeIn {
+        from {opacity: 0} 
+        to {opacity: 1}
     }
     
 </style>
