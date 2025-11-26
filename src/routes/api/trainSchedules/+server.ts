f8d549da6c741f837112adbee25c69939c16f456
RD-Tan <ross_tan@dlsu.edu.ph>
2025-11-27 02:16:50 +0800

    jericho's code amazing query 10/10 .💯

--- src/routes/api/trainSchedules/+server.ts
+++ src/routes/api/trainSchedules/+server.ts
@@ -8,61 +8,64 @@ export const GET: RequestHandler = async ({ url }) => {
 	const endStn = url.searchParams.get('to');
 	const timeStart = url.searchParams.get('timeStart');
 	const timeEnd = url.searchParams.get('timeEnd');
-	const timeType = url.searchParams.get('type')
+	const timeType = url.searchParams.get('type');
 
-	if(!fromStn || !endStn || !timeStart || !timeEnd || !timeType)
-		return new Response("Missing fields", {
-        status: 400
-    })
-	const from = getStationIndex(fromStn)
-	const to = getStationIndex(endStn)
-	const cls = "First"
+	if (!fromStn || !endStn || !timeStart || !timeEnd || !timeType)
+		return new Response('Missing fields', {
+			status: 400
+		});
+	const from = getStationIndex(fromStn);
+	const to = getStationIndex(endStn);
+	const cls = 'First';
 
-	const direction = from - to > 0 ? "Eastbound" : "Westbound"
+	const direction = from - to > 0 ? 'Eastbound' : 'Westbound';
 
-	console.log(from, to, fromStn, endStn, direction)
-	const tsStart = new Date(timeStart)
-	const tsEnd = new Date(timeEnd)
-	console.log(tsStart, tsEnd)
+	console.log(from, to, fromStn, endStn, direction);
+	const tsStart = new Date(timeStart);
+	const tsEnd = new Date(timeEnd);
+	console.log(tsStart, tsEnd);
 
-	const query = oltpdb.selectFrom("trains")
-		.innerJoin("journeys", "journeys.train_no", "trains.id")
-		.innerJoin("routes", "journeys.route", "routes.id")
-		.innerJoin("schedule", "schedule.journey_id", "journeys.id")
-		.innerJoin("tickets", "tickets.journey", "journeys.id")
-		.innerJoin("seat", "seat.id", "tickets.seat")
-		.innerJoin("cars", "cars.id", "seat.car")
-		.where("schedule.station", "in", [fromStn, endStn])
-		.where("tickets.class", "=", cls)
-		.where('journeys.id', 'in', (eb) =>
-			eb.selectFrom("journeys as sub_jny")
-				.innerJoin("routes as sub_rt", "sub_jny.route", "sub_rt.id")
-				.innerJoin("schedule as sub_schd", "sub_schd.journey_id", "sub_jny.id")
-				.select('sub_jny.id')
-				.where("sub_schd.departure", ">=", tsStart)
-				.where("sub_schd.departure", "<=", tsEnd)
-				.where("sub_schd.station", "=", fromStn)
-				.where("routes.direction", "=", direction)
-			)
-		.groupBy(["journeys.id", "cars.car_no", "tickets.class", "schedule.departure", 
-			"schedule.arrival", 
-			"journeys.train_no", 
-			"trains.capacity", 
-			"cars.seat_count",
-			"schedule.station"
+	const query = oltpdb
+		.selectFrom('trains')
+		.innerJoin('journeys', 'journeys.train_no', 'trains.id')
+		.innerJoin('routes', 'journeys.route', 'routes.id')
+		.innerJoin('schedule as dept_sched', 'dept_sched.journey_id', 'journeys.id')
+		.innerJoin('schedule as arriv_sched', 'arriv_sched.journey_id', 'journeys.id')
+		.leftJoin('tickets', 'tickets.journey', 'journeys.id')
+		.leftJoin('seat', 'seat.id', 'tickets.seat')
+		.leftJoin('cars', 'cars.id', 'seat.car')
+		.where('dept_sched.station', '=', fromStn)
+		.where('arriv_sched.station', '=', endStn)
+		.where('tickets.class', '=', cls)
+		.where('dept_sched.departure', '>=', tsStart)
+		.where('dept_sched.departure', '<=', tsEnd)
+		.where('routes.direction', '=', direction)
+		.groupBy([
+			'journeys.id',
+			'tickets.class',
+			'journeys.train_no',
+			'cars.car_no',
+			'cars.seat_count',
+			'dept_sched.departure',
+			'arriv_sched.arrival',
+			'journeys.id',
+			'dept_sched.id'
 		])
-		.select(["schedule.departure", 
-			"schedule.arrival", 
-			"journeys.train_no", 
-			"trains.capacity", 
-			"cars.seat_count",
-			"schedule.station",
-			(eb) => eb.fn.count<number>("cars.car_no").as("car_amount"),
-			(eb) => eb.fn.count<number>("tickets.class").as("occupancy")
-		])
+		.select([
+			'dept_sched.departure',
+			'arriv_sched.arrival',
+			'journeys.train_no',
+			'dept_sched.id',
+			(eb) =>
+				eb(
+					(oltpdb.fn.count<number>('cars.car_no'), '*', 'cars.seat_count'),
+					'-',
+					oltpdb.fn.count<number>('tickets.class')
+				).as('occupied')
+		]);
 
-	const res = await query.execute()
-	console.log(res)
-	
+	const res = await query.execute();
+	console.log(res);
+
 	return json(res);
 };
