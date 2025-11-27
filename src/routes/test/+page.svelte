365382b4389ceac0063469aed546a3a69bed4e0e
RD-Tan <ross_tan@dlsu.edu.ph>
2025-11-28 04:28:14 +0800

    just a page to run arbitrary fetch requests

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import type { BookingData } from '$lib/stores/booking';
+</script>
+
+<h1>hii</h1>
+<button
+	onclick={async () => {
+		const body: BookingData = {
+			train: '2',
+			classType: 'Economy',
+			stationFrom: 'Tokyo',
+			stationTo: 'Shin-Yokohama',
+			selectedSeats: [
+				{ car: 3, col: 1, row: 2 },
+				{ car: 3, col: 5, row: 2 }
+			],
+			journey: '62',
+			cost: 6969,
+			email: 'jerichoreyes@gmail.com',
+			cost_total: 6969,
+
+			// the following doesn't matter anymore
+			date: new Date().toISOString(),
+			filterType: 'what',
+			passengers: 1,
+			timeArrive: 'ther',
+			timeDepart: 'there',
+			timeStart: 'there'
+		};
+
+		console.log(JSON.stringify(body));
+		const res = await fetch('/api/bookings', {
+			method: 'POST',
+			body: JSON.stringify(body)
+		});
+
+		console.log(res, await res.json());
+	}}>this is a buttn</button
+>
