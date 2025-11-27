0cf6a8479361f0ec561fef639690f22dba898be1
jemire22 <jericho_reyes@dlsu.edu.ph>
2025-11-27 23:44:12 +0800

    small logic change

--- src/lib/data/stations.ts
+++ src/lib/data/stations.ts
@@ -12,5 +12,9 @@ export function getStations() {
 }
 
 export function getStationIndex(input:string) {
-	return stationMap.get(input) ?? 0;
+	return (stationMap.get(input) ?? 0) + 1
+}
+
+export function getDirection(from: number, to: number): string {
+	return from - to > 0 ? 'Eastbound' : 'Westbound';
 }
--- src/routes/api/trainSchedules/+server.ts
+++ src/routes/api/trainSchedules/+server.ts
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { oltpdb } from '$lib/db/oltpdb';
-import { getStationIndex } from '$lib/data/stations';
+import { getDirection, getStationIndex } from '$lib/data/stations';
 
 export const GET: RequestHandler = async ({ url }) => {
 	const fromStn = url.searchParams.get('from');
@@ -9,43 +9,35 @@ export const GET: RequestHandler = async ({ url }) => {
 	const timeStart = url.searchParams.get('timeStart');
 	const timeEnd = url.searchParams.get('timeEnd');
 	const timeType = url.searchParams.get('type');
-	const cls = url.searchParams.get('cls');
 
-	if (!fromStn || !endStn || !timeStart || !timeEnd || !timeType || !cls)
+	if (!fromStn || !endStn || !timeStart || !timeEnd || !timeType)
 		return new Response('Missing fields', {
 			status: 400
 		});
 	const from = getStationIndex(fromStn);
 	const to = getStationIndex(endStn);
 
-	const direction = from - to > 0 ? 'Westbound' : 'Eastbound';
+	const direction = getDirection(from, to)
 
 	console.log(from, to, fromStn, endStn, direction);
 	const tsStart = new Date(timeStart);
 	const tsEnd = new Date(timeEnd);
 	console.log(tsStart, tsEnd);
-
+	
 	const query = oltpdb
 		.selectFrom('trains')
 		.innerJoin('journeys', 'journeys.train_no', 'trains.id')
 		.innerJoin('routes', 'journeys.route', 'routes.id')
 		.innerJoin('schedule as dept_sched', 'dept_sched.journey_id', 'journeys.id')
 		.innerJoin('schedule as arriv_sched', 'arriv_sched.journey_id', 'journeys.id')
-		.leftJoin('tickets', 'tickets.journey', 'journeys.id')
-		.leftJoin('seat', 'seat.id', 'tickets.seat')
-		.leftJoin('cars', 'cars.id', 'seat.car')
-		.where('dept_sched.station', '=', fromStn)
-		.where('arriv_sched.station', '=', endStn)
-		.where('tickets.class', '=', cls)
-		.where('dept_sched.departure', '>=', tsStart)
-		.where('dept_sched.departure', '<=', tsEnd)
+		// .where('dept_sched.station', '=', fromStn)
+		// .where('arriv_sched.station', '=', endStn)
+		// .where('dept_sched.departure', '>=', tsStart)
+		// .where('dept_sched.departure', '<=', tsEnd)
 		.where('routes.direction', '=', direction)
 		.groupBy([
 			'journeys.id',
-			'tickets.class',
 			'journeys.train_no',
-			/* "cars.car_no", 
-			"cars.seat_count",*/
 			'dept_sched.departure',
 			'arriv_sched.arrival'
 		])
@@ -53,10 +45,10 @@ export const GET: RequestHandler = async ({ url }) => {
 			'journeys.id as name',
 			'dept_sched.departure as departs',
 			'arriv_sched.arrival as arrives',
-			'journeys.train_no as journey_id',
-			// 	(eb) => eb("cars.seat_count", "-", oltpdb.fn.count<number>("tickets.class")).as("occupied")
+			'journeys.train_no as journey_id'
 		]);
 
+	console.log(query.compile().sql)
 	const res = await query.execute();
 	console.log(res);
 
