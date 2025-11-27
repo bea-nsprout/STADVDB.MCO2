2f424a2b281b234a36951854e181c33b60126343
jemire22 <jericho_reyes@dlsu.edu.ph>
2025-11-27 15:39:47 +0800

    yes

--- src/routes/api/trainTickets/+server.ts
+++ src/routes/api/trainTickets/+server.ts
@@ -28,10 +28,12 @@ export const GET: RequestHandler = async ({ url }) => {
 		.where(eb => eb.or([
 			eb('tickets.origin', '<', from.toString()), 
 			eb('tickets.destination', '>', from.toString())
-		])).orderBy("cars.car_no", "asc")
+		]))
+		.orderBy("cars.id", "asc")
+		.orderBy("seat.column", "asc")
+		.orderBy("seat.row", "asc")
 		.select([
-			"cars.car_no",
-            "cars.seat_count",
+			"seat.car",
 			"seat.column",
             "seat.row"
         ]);
